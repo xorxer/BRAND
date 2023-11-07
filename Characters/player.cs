fdca24a67addacd0ea01f4422e4cda0d28c16be4
Anthony Le <le672869@gmail.com>
2023-11-07 13:56:47 -0800

    Created animations for player and basic movement

---
+++ Characters/player.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+	[Export]
+    public int Speed { get; set; } = 400;
+
+	public void GetInput() {
+		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+		Velocity = inputDirection * Speed;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
+}
