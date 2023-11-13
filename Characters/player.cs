6b95588cf053cb7b7e630d23ca202e3f0479f1c1
Anthony Le <le672869@gmail.com>
2023-11-12 19:36:47 -0800

    Player animations for every direction.

--- Characters/player.cs
+++ Characters/player.cs
@@ -10,35 +10,42 @@ public partial class Player : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		var playerDirection = Vector2.Zero; // The player's movement vector.
+		var player = GetNode<Sprite2D>("Body");
 
+		// Player moves in the direction of the input
+		// Where WASD are the arrow keys
 		if (Input.IsActionPressed("right"))
 		{
 			playerDirection.X += 1;
+			// Does not flip the sprite horizontally
+			player.FlipH = false;
 		}
-
 		if (Input.IsActionPressed("left"))
 		{
 			playerDirection.X -= 1;
+			// Flips the sprite horizontally
+			player.FlipH = true;
 		}
-
 		if (Input.IsActionPressed("down"))
 		{
 			playerDirection.Y += 1;
 		}
-
 		if (Input.IsActionPressed("up"))
 		{
 			playerDirection.Y -= 1;
 		}		
 
+		playerDirection = playerDirection.Normalized() * Speed;
+		Velocity = playerDirection;
+
+		var animationTree = GetNode<AnimationTree>("AnimationTree");
+		// AnimationNodeStateMachinePlayback stateMachine = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
 		if (playerDirection != Vector2.Zero)
 		{
-			playerDirection = playerDirection.Normalized() * Speed;
-			Velocity = playerDirection;
+			// stateMachine.Travel("Move");
+			animationTree.Set("parameters/Move/blend_position", playerDirection);
+			// animationTree.Set("parameters/conditions/Idle", playerDirection);
 			MoveAndSlide();
-			var animationTree = GetNode<AnimationTree>("AnimationTree");
-
-			animationTree.Set("parameters/", playerDirection);
 		}
 
 		
